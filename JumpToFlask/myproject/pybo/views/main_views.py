8c8e63952b311e556789ad05f771b664622114a2
phs2201 <phs2221@naver.com>
2022-12-22 13:02:13 +0900

    12.22web project

--- JumpToFlask/myproject/pybo/views/main_views.py
+++ JumpToFlask/myproject/pybo/views/main_views.py
@@ -11,7 +11,8 @@ def helloe_pybo():
 
 @bp.route('/')
 def index():
-    return redirect(url_for('question._list')) # url_for 함수에 전달 된 question._list는 question 이라는 별명을 가진 bp를 찾고
+    return redirect(url_for('question._list')) 
+# url_for 함수에 전달 된 question._list는 question 이라는 별명을 가진 bp를 찾고
 # 그 안에 _list르 등록된 함수명을 찾는다. question._list는 question 별칭으로 등록한 question_views.py 파일에서 _list함수를 의미한다.
 # _list함수에 등록된 URL 매핑 규칙은 @bp.route(’/list’)이므로 url_for(’question._list’)는 /question/list/ URL 주소를 반환한다.
     
